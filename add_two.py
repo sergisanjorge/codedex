2f2dbf3afb48a87fd9e9b48a4a2f0c2b1f9f7746
Sergi Sanjorge <sergisanjorge@gmail.com>
2026-05-06 13:52:59 +0200

    change float x int

--- add_two.py
+++ add_two.py
@@ -1,4 +1,4 @@
 # Program that asks for a number and adds 2 to it
-number = float(input("Enter a number: "))
+number = int(input("Enter a number: "))
 result = number + 2
 print(f"The result is: {result}")
